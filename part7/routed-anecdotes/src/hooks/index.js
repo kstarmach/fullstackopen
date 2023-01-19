4d1088440a6e6b16ed642792bf08ca5daad719b2
kstarmach <kamil.starmach@hotmail.com>
2023-01-19 22:21:30 +0100

    Removed nested repository

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const reset = () => {
+        setValue('')
+    }
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    return {
+        type,
+        value,
+        reset,
+        onChange
+    }
+}
+
+// modules can have several named exports
+export const useAnotherHook = () => {  // ...
+}
